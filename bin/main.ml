e730af71896e7d7615bf8dff691425175f7ff536
Guillaume <guillaume.thouvenin@vates.tech>
2025-03-25 20:14:55 +0100

    1.1 print a better error when conversion failed

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,17 @@
+exception Conversion_failure of string
+
+(* [int_of_string str] convert the string into an integer or raise
+   an exception Conversion_failure.
+  *)
+let int_of_string str =
+  try int_of_string str with Failure _ -> raise (Conversion_failure str)
+
 (* [sum_up acc] read a string until we read ctrl-D that is EOF.
    It convert the string to an int and add it to the accumulator.
    When EOF is read an exception is generated.
    It also raises an exception if it fails to convert the string
    to int.
- *)
+  *)
 let rec sum_up acc =
   let l = input_line stdin in
   acc := !acc + int_of_string l;
@@ -11,5 +19,7 @@ let rec sum_up acc =
 
 let () =
   let r = ref 0 in
-  try sum_up r
-  with End_of_file ->  Printf.printf "Sum is %d\n" !r
+  try sum_up r with
+  | End_of_file -> Printf.printf "Sum is %d\n" !r
+  | Conversion_failure s ->
+      Printf.fprintf stderr "Conversion failure of \"%s\"\n%!" s
